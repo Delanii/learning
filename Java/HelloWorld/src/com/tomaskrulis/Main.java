6b7cc2d7f11f2e85fc3a0152aec1b2dc08777fbd
Tomas Krulis <tomas.krulisintegromat.com>
2024-02-06 17:08:55 +0100

    Add Java Hello World and ignore Intellij metadata files.

---
+++ Java/HelloWorld/src/com/tomaskrulis/Main.java
@@ -0,0 +1,7 @@
+package com.tomaskrulis; // The package definition reflect the folder structure. Each dot is a subfolder, so the `Main.java` file has to be located in the path: `HelloWorld/src/com/tomaskrulis/Main.java`
+
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println("Hello World");
+        }
+}

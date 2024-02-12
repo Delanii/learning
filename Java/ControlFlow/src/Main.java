abdfa9ef1a7f5608b01754c504cc45e6aa141c2c
Tomas Krulis <tomas.krulisintegromat.com>
2024-02-12 14:08:27 +0100

    Finish the control flow lesson.

--- Java/ControlFlow/src/Main.java
+++ Java/ControlFlow/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -42,5 +44,58 @@ public class Main {
                 System.out.println("You are a guest");
             }
         }
+
+        // For loop
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Heyo" + i);
+        }
+
+        // While loop
+        int i = 0;
+        while (i < 5) {
+            System.out.println("Aloha" + i);
+            i++;
+        }
+        // User inputs + reference types comparison
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        // while (input != "quit")
+        /*
+        Doesn't work, because string is a reference type. Reference types return the memory address, so the comparison would compare the memory address of the `input` variable with the memory address of the temporary variable that holds the `quit` string.
+         */
+        while (!input.equals("quit")) {
+            System.out.println("Input: ");
+            input = scanner.nextLine()
+                    .trim()
+                    .toLowerCase();
+            System.out.println(input);
+        }
+        // A better version -- the loop ends only with the input "quit"
+        while (true) {
+            System.out.println("Input: ");
+            input = scanner.nextLine()
+                    .trim()
+                    .toLowerCase();
+            if (input.equals("continue"))
+                continue; // Continue resets the control flow back to the start of the loop
+            if (input.equals("quit"))
+                break; // Don't print "quit" when the `input` is "quit"
+            System.out.println(input);
+        }
+        // Do-while
+        // The `do` code block runs before Java evaluates the `while` condition; so the `do` code block runs always. In contrast, the `while` loop doesn't have to run if it's loop condition evaluates to `false` before the first iteration.
+        do {
+            System.out.println("Input: ");
+            input = scanner.nextLine()
+                    .trim()
+                    .toLowerCase();
+            System.out.println(input);
+        } while (!input.equals("quit"));
+        // For-each
+        String[] fruits = {"Orange", "Mango", "Papaya"};
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        } // The type of the iterating variable `fruit` should match the type of the items in the iterated array `fruits`
+        // There's no access to the index of each item in the for-each loop; the for loop has access to the index of array items.
     }
 }

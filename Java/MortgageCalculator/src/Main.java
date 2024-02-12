bc721a9dd14b51d3c9bac20b101b59539f36e2a7
Tomas Krulis <tomas.krulisintegromat.com>
2024-02-12 20:20:07 +0100

    Refactor the mortgage calculator.

--- Java/MortgageCalculator/src/Main.java
+++ Java/MortgageCalculator/src/Main.java
@@ -4,6 +4,21 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
+    // Principal
+    private static final int PRINCIPAL_MIN = 100_000;
+    private static final String PRINCIPAL_MIN_STRING = "100k";
+    private static final int PRINCIPAL_MAX = 100_000_000;
+    private static final String PRINCIPAL_MAX_STRING = "100M";
+
+    // Annual interest rate
+    private static final double ANNUAL_INTEREST_RATE_MIN = 0;
+    private static final double ANNUAL_INTEREST_RATE_MAX = 10;
+
+    // Period
+    private static final byte PERIOD_YEARS_MIN = 1;
+    private static final byte PERIOD_YEARS_MAX = 30;
+
+
     public static void main(String[] args) {
 
         // Initialize reading user input
@@ -12,49 +27,75 @@ public class Main {
         /*
          Prompt user
          */
+
         // Principal
-        final int PRINCIPAL_MIN = 100_000;
-        final String PRINCIPAL_MIN_STRING = "100k";
-        final int PRINCIPAL_MAX = 100_000_000;
-        final String PRINCIPAL_MAX_STRING = "100M";
-
-        int principal = 0;
-
-        while (true) {
-            System.out.print("Principal (" + PRINCIPAL_MIN_STRING + " - " + PRINCIPAL_MAX_STRING + "): ");
-            principal = Integer.parseInt(scanner.nextLine().trim());
-            if (principal >= PRINCIPAL_MIN && principal <= PRINCIPAL_MAX )
-                break;
-            System.out.println("Principal must be a number between " + PRINCIPAL_MIN + " and " + PRINCIPAL_MAX + ".");
-        }
+        int principal = (int)readInput(scanner,
+                "Principal (" + PRINCIPAL_MIN_STRING + " - " + PRINCIPAL_MAX_STRING + "): ",
+                "principal",
+                "Principal must be a number between " + PRINCIPAL_MIN + " and " + PRINCIPAL_MAX + ".");
 
         // Annual interest rate
-        final double ANNUAL_INTEREST_RATE_MIN = 0;
-        final double ANNUAL_INTEREST_RATE_MAX = 10;
-
-        double annualInterestRatePercent = 0;
-
-        while (true) {
-            System.out.print("Annual interest rate: ");
-            annualInterestRatePercent = Double.parseDouble(scanner.nextLine().trim());
-            if (annualInterestRatePercent > ANNUAL_INTEREST_RATE_MIN && annualInterestRatePercent <= ANNUAL_INTEREST_RATE_MAX)
-                break;
-            System.out.println("Annual interest rate must be a number between " + ANNUAL_INTEREST_RATE_MIN + " and " + ANNUAL_INTEREST_RATE_MAX + ".");
-        }
+        double annualInterestRatePercent = readInput(scanner,
+                "Annual interest rate: ",
+                "annualInterestRate",
+                "Annual interest rate must be a number between " + ANNUAL_INTEREST_RATE_MIN + " and " + ANNUAL_INTEREST_RATE_MAX + ".");
 
         // Period
-        final byte PERIOD_YEARS_MIN = 1;
-        final byte PERIOD_YEARS_MAX = 30;
+        byte periodYears = (byte)readInput(scanner,
+                "Period (in Years): ",
+                "periodYears",
+                "Period must be a number between " + PERIOD_YEARS_MIN + " and " + PERIOD_YEARS_MAX + ".");
 
-        byte periodYears = 0;
+        double monthlyPay = calculateMortgage(principal,
+                annualInterestRatePercent,
+                periodYears);
 
+        // Show the result
+        String showMonthlyPay = NumberFormat.getCurrencyInstance().format(monthlyPay);
+        System.out.println("Mortgage: " + showMonthlyPay);
+    }
+
+    public static double readInput(Scanner scanner,
+                                   String prompt,
+                                   String type,
+                                   String errorMessage) {
+        double input; // Initialization can be even without an initial value
         while (true) {
-            System.out.print("Period (in Years): ");
-            periodYears = Byte.parseByte(scanner.nextLine().trim());
-            if (periodYears > PERIOD_YEARS_MIN && periodYears <= PERIOD_YEARS_MAX)
+            System.out.print(prompt);
+            input = Double.parseDouble(scanner.nextLine().trim());
+            boolean isValid = validateInput(input, type);
+            if (isValid)
                 break;
-            System.out.println("Period must be a number between " + PERIOD_YEARS_MIN + " and " + PERIOD_YEARS_MAX + ".");
+            System.out.println(errorMessage);
         }
+        return input;
+    }
+
+    public static boolean validateInput(double input, String type) {
+        boolean isValid = false;
+        switch (type) {
+            case "principal": {
+                int principal = (int)input;
+                isValid = (principal >= PRINCIPAL_MIN) && (principal <= PRINCIPAL_MAX);
+                break;
+            }
+            case "annualInterestRate": {
+                double annualInterestRatePercent = input;
+                isValid = (annualInterestRatePercent > ANNUAL_INTEREST_RATE_MIN) && (annualInterestRatePercent <= ANNUAL_INTEREST_RATE_MAX);
+                break;
+            }
+            case "periodYears": {
+                byte periodYears = (byte)input;
+                isValid = (periodYears > PERIOD_YEARS_MIN) && (periodYears <= PERIOD_YEARS_MAX);
+                break;
+            }
+        }
+        return isValid;
+    }
+
+    public static double calculateMortgage(int principal,
+                                           double annualInterestRatePercent,
+                                           byte periodYears) {
 
         // Utility calculations
         double monthlyInterestRate = (annualInterestRatePercent / 100) / 12;
@@ -63,8 +104,6 @@ public class Main {
         // Monthly pay calculation
         double monthlyPay = principal * ( monthlyInterestRate * incrementer ) / ( incrementer - 1 );
 
-        // Show the result
-        String showMonthlyPay = NumberFormat.getCurrencyInstance().format(monthlyPay);
-        System.out.println("Mortgage: " + showMonthlyPay);
+        return monthlyPay;
     }
 }

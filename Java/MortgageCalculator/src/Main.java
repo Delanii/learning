183757911e31f6984e11f7918096724c8e51a234
Tomas Krulis <tomas.krulisintegromat.com>
2024-02-18 17:41:43 +0100

    Finished mortgage calculator V2.

--- Java/MortgageCalculator/src/Main.java
+++ Java/MortgageCalculator/src/Main.java
@@ -46,13 +46,19 @@ public class Main {
                 "periodYears",
                 "Period must be a number between " + PERIOD_YEARS_MIN + " and " + PERIOD_YEARS_MAX + ".");
 
-        double monthlyPay = calculateMortgage(principal,
+        double monthlyPay = calculateMonthlyPay(principal,
                 annualInterestRatePercent,
                 periodYears);
 
-        // Show the result
-        String showMonthlyPay = NumberFormat.getCurrencyInstance().format(monthlyPay);
-        System.out.println("Mortgage: " + showMonthlyPay);
+        // Show the monthly pay result
+        showMonthlyPay(principal,
+                annualInterestRatePercent,
+                periodYears);
+
+        // Show payment schedule
+        showPaymentSchedule(principal,
+                annualInterestRatePercent,
+                periodYears);
     }
 
     public static double readInput(Scanner scanner,
@@ -93,17 +99,75 @@ public class Main {
         return isValid;
     }
 
-    public static double calculateMortgage(int principal,
+    public static double calculateMonthlyInterestRate(double annualInterestRatePercent) {
+        double monthlyInterestRate = (annualInterestRatePercent / 100) / 12;
+        return monthlyInterestRate;
+    }
+
+    public static short calculateNumberOfPayments(byte periodYears) {
+        short payments = (short)(periodYears * 12);
+        return payments;
+    }
+
+    public static double calculateMonthlyPay(int principal,
                                            double annualInterestRatePercent,
                                            byte periodYears) {
 
         // Utility calculations
-        double monthlyInterestRate = (annualInterestRatePercent / 100) / 12;
+        double monthlyInterestRate = calculateMonthlyInterestRate(annualInterestRatePercent);
         double incrementer = Math.pow(1 + monthlyInterestRate, periodYears * 12);
 
         // Monthly pay calculation
         double monthlyPay = principal * ( monthlyInterestRate * incrementer ) / ( incrementer - 1 );
 
         return monthlyPay;
+    }
+
+    public static void showHeading(String title) {
+        String header = "--------";
+
+        System.out.println(title);
+        System.out.println(header);
+        System.out.print("\n");
+    }
+
+    public static void showMonthlyPay(int principal,
+                                      double annualInterestRatePercent,
+                                      byte periodYears) {
+        showHeading("Mortgage");
+
+        double monthlyPay = calculateMonthlyPay(principal,
+                annualInterestRatePercent,
+                periodYears);
+        String showMonthlyPay = NumberFormat.getCurrencyInstance().format(monthlyPay);
+        System.out.println("Monthly payments: " + showMonthlyPay);
+    }
+
+    public static double calculateRemainder(int principal,
+                                            double annualInterestRatePercent,
+                                            byte periodYears,
+                                            byte remainingNumberOfPayments) {
+
+        // Utility calculations
+        double monthlyInterestRate = calculateMonthlyInterestRate(annualInterestRatePercent);
+        double normalizedInterest = 1 + monthlyInterestRate;
+
+        double remainder = principal *
+                (Math.pow(normalizedInterest, periodYears * 12) - (Math.pow(normalizedInterest, remainingNumberOfPayments))) /
+                (Math.pow(normalizedInterest, periodYears * 12) - 1);
+
+        return remainder;
+    }
+
+    public static void showPaymentSchedule(int principal,
+                                           double annualInterestRatePercent,
+                                           byte periodYears) {
+        showHeading("Payment schedule");
+
+        for(byte i = 1; i <= calculateNumberOfPayments(periodYears); i++) {
+           double remainder = calculateRemainder(principal,
+                   annualInterestRatePercent, periodYears, i);
+           System.out.println(NumberFormat.getCurrencyInstance().format(remainder));
+        }
     }
 }

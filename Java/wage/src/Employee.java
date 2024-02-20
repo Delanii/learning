24716e8ed0a85b77c179a995c5753a82b2846477
Tomas Krulis <tomas.krulisintegromat.com>
2024-02-20 19:14:25 +0100

    Add getters and setters.

--- Java/wage/src/Employee.java
+++ Java/wage/src/Employee.java
@@ -1,7 +1,29 @@
 public class Employee {
 
-    public int baseSalary = 0;
-    public int hourlyRate = 0;
+    private int baseSalary = 0;
+    private int hourlyRate = 0;
+
+    public void setBaseSalary(int baseSalary){
+        if (baseSalary <= 0) {
+            throw new IllegalArgumentException("Salary has to be more than zero."); // throwing error terminates the program
+        }
+        this.baseSalary = baseSalary; // So this happens if the above is `false`
+    }
+
+    public int getBaseSalary(){
+        return baseSalary;
+    }
+
+    public void setHourlyRate(int hourlyRate){
+        if (hourlyRate <= 0) {
+            throw new IllegalArgumentException("Hourly rate has to be more than zero."); // throwing error terminates the program
+        }
+        this.hourlyRate = hourlyRate; // So this happens if the above is `false`
+    }
+
+    public int getHourlyRate(){
+        return hourlyRate;
+    }
 
     public int calculateWage(int extraHours){ // it's not necessary to add the class fields as parameters, since they are always present in the classes code ...!
         return baseSalary + (extraHours * hourlyRate);
--- Java/wage/src/Main.java
+++ Java/wage/src/Main.java
@@ -3,8 +3,8 @@ public class Main {
 
         var employee = new Employee();
 
-        employee.baseSalary = 50_000;
-        employee.hourlyRate = 20;
+        employee.setBaseSalary(50_000);
+        employee.setHourlyRate(20);
         int wage = employee.calculateWage(10);
 
        System.out.println(wage);

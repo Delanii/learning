db65bf2777ff23458fd14ca11f5f16bf076063ce
Tomas Krulis <tomas.krulisintegromat.com>
2024-02-20 18:34:22 +0100

    Creating new objects and working with references.

--- Java/HelloWorld/src/com/tomaskrulis/Main.java
+++ Java/HelloWorld/src/com/tomaskrulis/Main.java
@@ -2,6 +2,17 @@ package com.tomaskrulis; // The package definition reflect the folder structure.
 
     public class Main {
         public static void main(String[] args) {
-            System.out.println("Hello World");
+
+            TextBox textBox1 = new TextBox();
+            textBox1.setText("Box 1");
+            System.out.println(textBox1.text);
+
+            var textBox2 = new TextBox();
+            textBox2.setText("Box 2");
+            System.out.println(textBox2.text);
+
+            var textBox3 = textBox1; // `textBox3` is set to the same type (`TextBox`) and has the same contents as `textBox1`. `textBox1` contains the reference to the object in memory, so both of the object point to the same part of the memory.
+            textBox3.setText("Change!");
+            System.out.println(textBox1.text); // The `textBox1` and `textBox3` reference the same object.
         }
 }
--- Java/HelloWorld/src/com/tomaskrulis/TextBox.java
+++ Java/HelloWorld/src/com/tomaskrulis/TextBox.java
@@ -2,7 +2,7 @@ package com.tomaskrulis;
 
 public class TextBox {
 
-    public String text; // a class field
+    public String text = ""; // a class field initialized to empty string to prevent the `NullPointerException`. If the field is not initialized and someone tries to use it, they get a null pointer ... :D The field doesn't exist in the memory yet.
 
     public void setText(String text) {
         this.text = text; // the `text` field of the TextBox object is set to the value passed to the `setText` method

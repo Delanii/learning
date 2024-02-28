d2c56c65654446aebff77d6cd6a0185a79ce3533
Tomas Krulis <tomas.krulisintegromat.com>
2024-02-28 18:29:06 +0100

    Add method overloading and static member example.

--- Java/wage/src/Employee.java
+++ Java/wage/src/Employee.java
@@ -3,10 +3,13 @@ public class Employee {
     private int baseSalary;
     private int hourlyRate;
 
+    public static int numberOfEmployees;
+
     public Employee(int baseSalary,
                     int hourlyRate) { // class constructor doesn't have a return type
         setBaseSalary(baseSalary);   // If the field has a setter, use that to leverage the validation that is part of the setter
         setHourlyRate(hourlyRate);
+        numberOfEmployees++;
     }
 
     private void setBaseSalary(int baseSalary){
@@ -33,5 +36,16 @@ public class Employee {
 
     public int calculateWage(int extraHours){ // it's not necessary to add the class fields as parameters, since they are always present in the classes code ...!
         return baseSalary + (extraHours * hourlyRate);
+    }
+
+    public int calculateWage(){ // This method is overloaded from the method with parameters. Java doesn't have optional parameters, and method overloading is one of the options how to achieve the behavior.
+        // In addition, Java doesn't allow to create a method with default values for parameters
+        return calculateWage(0);
+    }
+
+    public static void showNumberOfEmployees() {
+        System.out.println(Employee.numberOfEmployees);
+        // calculateWage // You can't access a instance member from a static member directly. To access instance member methods, you have to create a class instance first:
+        // new Employee().
     }
 }

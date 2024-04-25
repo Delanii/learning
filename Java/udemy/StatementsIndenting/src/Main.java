378aff4450da177fcb2051180e79aa5014087ed5
Tomas Krulis <krulis.tomas@seznam.cz>
2024-04-25 22:22:19 +0200

    Add statements lesson.

---
+++ Java/udemy/StatementsIndenting/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myVariable = 50;
+
+        myVariable++;
+
+        System.out.println("This is a statement as well");
+        System.out.println("Even on" +
+                "multiple lines");
+    }
+}

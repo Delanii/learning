84f44852a44d9cd403cee342088bb02faf06d157
Tomas Krulis <tomas.krulisintegromat.com>
2024-08-08 12:13:14 +0200

    Add the Profile component.

--- JavaScript/frameworks/reactjs/officialTutorial/first-page/src/App.jsx
+++ JavaScript/frameworks/reactjs/officialTutorial/first-page/src/App.jsx
@@ -1,3 +1,9 @@
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
 function MyButton() {
   return (
     <button>
@@ -6,11 +12,29 @@ function MyButton() {
   );
 }
 
+function Profile() {
+  return (
+    <>
+    <h2>{user.name}</h2>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        syle={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  )
+}
+
 export default function MyApp() {
   return (
     <div>
       <h1>Welcome to my page</h1>
       <MyButton />
+      <Profile />
     </div>
   );
 }

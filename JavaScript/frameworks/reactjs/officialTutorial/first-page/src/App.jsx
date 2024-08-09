0abcee1e55f4a0c09a171f227a1d1b9c69fe12fc
Tomas Krulis <tomas.krulisintegromat.com>
2024-08-09 10:59:02 +0200

    Add the shopping list component.

--- JavaScript/frameworks/reactjs/officialTutorial/first-page/src/App.jsx
+++ JavaScript/frameworks/reactjs/officialTutorial/first-page/src/App.jsx
@@ -29,12 +29,38 @@ function Profile() {
   )
 }
 
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+function ShoppingList() {
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+    {product.title}
+    </li>
+  );
+
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  );
+}
+
 export default function MyApp() {
   return (
     <div>
       <h1>Welcome to my page</h1>
       <MyButton />
       <Profile />
+      <ShoppingList />
     </div>
   );
 }
